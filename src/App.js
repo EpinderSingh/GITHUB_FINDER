501bebb2db9fa9f3d448e69907239c60f6ffe4b4
Epinder <epinder.singh@gmail.com>
2021-02-16 18:40:38 +0800

    fetching data in componentDidmount lifecycle

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ class App extends Component {
   };
 
   async componentDidMount() {
+    this.setState({ loading: true });
+
     const res = await axios.get('https://api.github.com/users');
-    console.log(res.data);
+    this.setState({ users: res.data, loading: false });
   }
 
   render() {
@@ -21,7 +23,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
